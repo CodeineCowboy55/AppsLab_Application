90aab5bd1bf80a00fc1090ece3039ef2cedf1e16
jm <jmrrocks55@gmail.com>
2024-01-30 17:11:12 +0000

    This version now has some hard coded data implemented

--- venv/Data.py
+++ venv/Data.py
@@ -1,2 +1,123 @@
-#This is the Page where all the Data that the app generate and containts will be placed
+import datetime
 
+Project1 = {
+    "Request Date": "02/02/2024",
+    "Date Required": "06/04/2024",
+    "User": "Mia",
+    "Customer Number": 2546984,
+    "Project Number": 5648795,
+    "Fragrance Number": 25698547,
+    "Dosage": 0.5,
+    "Product Type": "EDT",
+    "Base": True,
+}
+
+Project2 = {
+    "Request Date": "10/01/2024",
+    "Date Required": "08/02/2024",
+    "User": "Bruce",
+    "Customer Number": 4569825,
+    "Project Number": 125478,
+    "Fragrance Number": 69854751,
+    "Dosage": 1.6,
+    "Product Type": "Room Spray",
+    "Base": False,
+}
+
+Project3 = {
+    "Request Date": "15/03/2024",
+    "Date Required": "25/04/2024",
+    "User": "Lindsey",
+    "Customer Number": 452168,
+    "Project Number": 395216,
+    "Fragrance Number": 4698215,
+    "Dosage": 4.6,
+    "Product Type": "candle",
+    "Base": True,
+}
+
+Project4 = {
+    "Request Date": "12/02/2024",
+    "Date Required": "19/03/2024",
+    "User": "Mia",
+    "Customer Number": 162548,
+    "Project Number": 974216,
+    "Fragrance Number": 7563219,
+    "Dosage": 6.5,
+    "Product Type": "wax melt",
+    "Base": 0.5,
+}
+
+Project5 = {
+    "Request Date": "13/01/2024",
+    "Date Required": "18/04/2024",
+    "User": "Harry",
+    "Customer Number": 165289,
+    "Project Number": 652158,
+    "Fragrance Number": 1247854,
+    "Dosage": 8.9,
+    "Product Type": "conditioner",
+    "Base": False,
+}
+
+Project6 = {
+    "Request Date": "05/02/2024",
+    "Date Required": "15/02/2024",
+    "User": "Sam",
+    "Customer Number": 147528,
+    "Project Number": 942357,
+    "Fragrance Number": 1254239,
+    "Dosage": 12,
+    "Product Type": "Polish",
+    "Base": False,
+}
+
+Project7 = {
+    "Request Date": "12/02/2024",
+    "Date Required": "28/02/2024",
+    "User": "Bruce",
+    "Customer Number": 489279,
+    "Project Number": 164852,
+    "Fragrance Number": 7159349,
+    "Dosage": 2.6,
+    "Product Type": "EDP",
+    "Base": True,
+}
+
+Project8 = {
+    "Request Date": "04/03/2024",
+    "Date Required": "15/03/2024",
+    "User": "Sam",
+    "Customer Number": 456852,
+    "Project Number": 951458,
+    "Fragrance Number": 4815974,
+    "Dosage": 10,
+    "Product Type": "Candle",
+    "Base": False,
+}
+
+Project9 = {
+    "Request Date": "25/04/2024",
+    "Date Required": "12/05/2024",
+    "User": "Mia",
+    "Customer Number": 584965,
+    "Project Number": 136548,
+    "Fragrance Number": 2089064,
+    "Dosage": 0.9,
+    "Product Type": "Room spray",
+    "Base": True,
+}
+
+Project10 = {
+    "Request Date": "30/01/2024",
+    "Date Required": "18/02/2024",
+    "User": "Sam",
+    "Customer Number": 102654,
+    "Project Number": 603254,
+    "Fragrance Number": 2040856,
+    "Dosage": 1.8,
+    "Product Type": "EDT",
+    "Base": True,
+}
+
+Project_Dictionary = {1: Project1, 2: Project2, 3: Project3, 4: Project4, 5: Project5, 6: Project6, 7: Project7, 8: Project8, 9: Project9, 10: Project10}
