1a7307e53640603157dbdc3e0af241b68f584d9b
JM <jean-michel.ramar@effgroup.com>
2024-02-13 22:22:00 +0000

    The most current version

--- venv/Admin.py
+++ venv/Admin.py
@@ -1,16 +1,16 @@
-#This is where the admin  password and delete record definition is kept
+#This definition allows Lab manager to enter the admin password and delete record provided the ID exsists
 from Data import my_projectdict
 
 def delete_record():
     try:
-        # Enter the password to allow deletion
+        # Enter the password to allow deletion Manually coded password
         password = input("Enter the password to delete records: ")
 
         # Check if the password is correct
         if password == "Gabagoo13!":  #This is the password enter be able to delete a record only admin should know password
             id_tofetch = int(input("Enter the ID to fetch record: "))
-
-            # Check if the ID exists
+            #This part of the Definition will fetch ID from Project dictionary then check if it exsists, if its does will delete the selected ID and all data connected to it,
+            # Checks if the ID user is trying to load
             if id_tofetch in my_projectdict:
                 del my_projectdict[id_tofetch]
                 print(f'DELETED {id_tofetch}')
